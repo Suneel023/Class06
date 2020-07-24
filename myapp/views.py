2c53cf5347fd6e32ea9d835a7b309fe02474a44b
Suneel023 <mariochill@yahoo.com>
2020-07-24 23:30:13 +0530

    MyApp URL Config

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from math import factorial
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Welcome to Views of MyApp</h1>")
+
+def home(request):
+    return render(request,"myapp/home.html",{'name':"Sunil"})
+
+def fact(request,n):
+    n=int(n)
+    return HttpResponse("<h4>factorial of {} is {}</h4>".format(n,factorial(n)))
+
